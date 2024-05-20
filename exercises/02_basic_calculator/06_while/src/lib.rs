bb6aec0cad8d5d9071ebaff16771069e925a49d9
edoch <edo_chio@hotmail.it>
2024-05-21 00:07:49 +0200

    2 completed

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut total: u32 = 1;
+    let mut actual_n: u32 = n;
+
+    while actual_n > 0 {
+        total = total*actual_n;
+        actual_n = actual_n-1;
+    }
+    return total;
 }
 
 #[cfg(test)]
